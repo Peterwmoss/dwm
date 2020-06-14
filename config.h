0c213d42dfbeded2c8880f5781d7bdeeee2a418e
Peter K. Moss <peter@moss.dk>
2020-06-14 16:19:28 +0200

    Updated

--- config.h
+++ config.h
@@ -20,17 +20,17 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "TeamSpeak 3",  NULL,       NULL,       0,       1,           2 },
+	/* class            instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",           NULL,       NULL,       0,            1,           -1 },
+	{ "TeamSpeak 3",    NULL,       NULL,       0,            1,            2 },
+	{ "spotify",        NULL,       NULL,       8,            0,           -1 },
 };
 
 /* layout(s) */
