7826accabb5c2c2899b766ba60b72bdb8467a3c1
Peter K. Moss <peter@moss.dk>
2020-08-02 16:17:07 +0200

    Updated font size

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx = 30;   /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"Hack Nerd Font Mono:size=10"};
+static const char *fonts[] = {"Hack Nerd Font Mono:size=11"};
 static char normbgcolor[] = "#222222";
 static char normbordercolor[] = "#444444";
 static char normfgcolor[] = "#bbbbbb";
--- dwm.c
+++ dwm.c
@@ -728,11 +728,9 @@ void drawbar(Monitor *m) {
   Client *c;
 
   /* draw status first so it can be overdrawn by tags later */
-  if (m == selmon) { /* status is only drawn on selected monitor */
-    drw_setscheme(drw, scheme[SchemeNorm]);
-    tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
-    drw_text(drw, m->ww - tw, 0, tw, bh, 0, stext, 0);
-  }
+  drw_setscheme(drw, scheme[SchemeNorm]);
+  tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
+  drw_text(drw, m->ww - tw, 0, tw, bh, 0, stext, 0);
 
   for (c = m->clients; c; c = c->next) {
     occ |= c->tags;
@@ -1963,9 +1961,11 @@ void updatesizehints(Client *c) {
 }
 
 void updatestatus(void) {
+  Monitor *m;
   if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
     strcpy(stext, "dwm-" VERSION);
-  drawbar(selmon);
+  for (m = mons; m; m = m->next)
+    drawbar(m);
 }
 
 void updatetitle(Client *c) {
